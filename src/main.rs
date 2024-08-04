1aac854640930f3abf0afa451803019e8b98d66f
brennschlus <brennschluss@protonmail.com>
2024-08-04 22:27:10 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,78 @@
 use std::env;
+use std::fmt::Display;
 use std::fs;
 use std::io::{self, Write};
+use std::str::FromStr;
+
+#[derive(Clone, Copy)]
+enum TokenType {
+    LeftParen,
+    RightParen,
+    EOF,
+}
+struct TokenTypeParseError;
+impl FromStr for TokenType {
+    type Err = TokenTypeParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "" | "\n" => Ok(TokenType::EOF),
+            "(" => Ok(TokenType::LeftParen),
+            ")" => Ok(TokenType::RightParen),
+            _ => Err(TokenTypeParseError),
+        }
+    }
+}
+
+impl TokenType {
+    fn to_lexeme(&self) -> String {
+        match self {
+            &TokenType::LeftParen => "(".to_owned(),
+            &TokenType::RightParen => ")".to_owned(),
+            &TokenType::EOF => "".to_owned(),
+        }
+    }
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match &self {
+            TokenType::EOF => "EOF",
+            TokenType::LeftParen => "LEFT_PAREN",
+            TokenType::RightParen => "RIGHT_PAREN",
+        };
+        write!(f, "{}", name)
+    }
+}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} null", self.token_type, self.lexeme)
+    }
+}
+
+impl Token {
+    fn new(token_type: TokenType) -> Token {
+        Token {
+            token_type,
+            lexeme: token_type.to_lexeme(),
+        }
+    }
+}
+
+fn tokenize(input: String) {
+    for token in input.chars() {
+        let token_type = TokenType::from_str(&token.to_string());
+        match token_type {
+            Ok(token_type) => println!("{}", Token::new(token_type)),
+            Err(_) => eprintln!("wrong token"),
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,12 +91,7 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            }
+            tokenize(file_contents);
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
